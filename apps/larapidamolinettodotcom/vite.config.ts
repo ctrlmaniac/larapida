90614c3c5614d238f3cbb8182938566d7929727f
Davide Di Criscito <davide.dicriscito@gmail.com>
2025-04-02 19:49:39 +0200

    feat(larapidamolinettodotnet): init

---
+++ apps/larapidamolinettodotcom/vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import { reactRouter } from '@react-router/dev/vite';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/apps/larapidamolinettodotcom',
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+  plugins: [!process.env.VITEST && reactRouter()],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+}));
