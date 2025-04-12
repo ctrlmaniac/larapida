1d9106b7f254229d02ebcb48fe3ed463297b9a83
Davide Di Criscito <davide.dicriscito@gmail.com>
2025-04-13 00:48:03 +0200

    feat(server): aggiunge compression e morgan

--- apps/server/main.ts
+++ apps/server/main.ts
@@ -1,9 +1,15 @@
 import express, { type Application } from 'express';
+import compression from 'compression';
+import morgan from 'morgan';
 import appConfig from './config';
 import { join } from 'path';
 import ssrAppLoader from './helpers/ssrAppLoader';
 
 const app: Application = express();
+
+app.disable('x-powered-by');
+app.use(compression());
+app.use(morgan('tiny'));
 
 /** WWW hydrated client and static files */
 app.use('/assets', express.static(join(__dirname, 'www/client/assets')));
