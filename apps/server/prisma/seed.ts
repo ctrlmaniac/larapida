49471d2c2a228197e2b84890cc50ff399f1d4935
Davide Di Criscito <davide.dicriscito@gmail.com>
2025-04-16 18:42:59 +0200

    feat: add seed

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -1,6 +1,41 @@
 import { PrismaClient } from './prisma';
 import * as bcrypt from 'bcrypt';
 
+const categories = [
+  {
+    name: 'Riparazioni',
+    slug: 'riparazioni',
+  },
+  {
+    name: 'Duplicazione Chiavi',
+    slug: 'chiavi',
+  },
+  {
+    name: 'Duplicazione Chiavi Auto',
+    slug: 'chiavi-auto',
+  },
+  {
+    name: 'Gusci Chiavi Auto',
+    slug: 'gusci-chiavi-auto',
+  },
+  {
+    name: 'Duplicazione Radiocomandi',
+    slug: 'radiocomandi',
+  },
+  {
+    name: 'Batterie per orologi e altro',
+    slug: 'batterie',
+  },
+  {
+    name: 'Targhe',
+    slug: 'targhe',
+  },
+  {
+    name: 'Accessori calzature',
+    slug: 'accessori-calzature',
+  },
+];
+
 const prisma = new PrismaClient();
 async function main() {
   const passwordHash = bcrypt.hashSync(process.env.SU_PASS, 10);
@@ -18,6 +53,19 @@ async function main() {
   });
 
   console.log({ superuser });
+
+  for (const category of categories) {
+    const newCategory = await prisma.category.upsert({
+      where: { slug: category.slug },
+      update: {},
+      create: {
+        name: category.name,
+        slug: category.slug,
+      },
+    });
+
+    console.log({ newCategory });
+  }
 }
 
 main()
